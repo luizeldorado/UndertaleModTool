fba60e08ffd348f95a791bf0bbeb24374d956fb1
luizzeroxis <eldorado.luiz@gmail.com>
2024-09-06 21:08:22 -0300

    Updates for search in code window
    Fix loader dialog not closing when caching is used
    Fix result counts
    Results actually sorted
    Only one window allowed
    Focus on textbox when opening
    
    (cherry picked from commit fe73d6ebfc7f68620423177aceb4f0083d753f5b)

--- UndertaleModTool/MainWindow.xaml.cs
+++ UndertaleModTool/MainWindow.xaml.cs
@@ -943,10 +943,15 @@ namespace UndertaleModTool
             GoForward();
         }
 
+        private SearchInCodeWindow searchInCodeWindow;
         private void Command_SearchInCode(object sender, ExecutedRoutedEventArgs e)
         {
-            SearchInCodeWindow w = new();
-            w.Show();
+            if (searchInCodeWindow is null)
+            {
+                searchInCodeWindow = new();
+                searchInCodeWindow.Show();
+            }
+            searchInCodeWindow.ActivateAndFocusOnTextBox();
         }
 
         private void DisposeGameData()
